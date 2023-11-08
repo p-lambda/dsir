a83705d13e0ddeac0e5d7796c20f1a43eeaf1119
Michael Xie <sangxie513@gmail.com>
2023-11-08 10:48:04 -0500

    bump version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 if __name__ == "__main__":
     setup(name='data-selection',
-          version='1.0.0',
+          version='1.0.1',
           description='Data Selection with Importance Resampling',
           url='https://github.com/p-lambda/dsir',
           author='Sang Michael Xie',
